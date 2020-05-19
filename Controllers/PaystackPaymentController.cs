2647cc586a141aaeb954732b3b25e14eabd94f8f
Elliott <Elliott@VR>
2020-05-19 20:21:54 +0100

    Add .gitignore and .gitattributes.

---
+++ Controllers/PaystackPaymentController.cs
@@ -0,0 +1,38 @@
+﻿using PaystackPayment.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Paystack.Net.SDK.Transactions;
+
+namespace PaystackPayment.Controllers
+{
+    public class PaystackPaymentController : Controller
+    {
+        // GET: PaystackPayment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<JsonResult> InitializePayment(PaystackCustomerModel model)
+        {
+            string secretKey = ConfigurationManager.AppSettings["PaystackSecret"];
+            var paystackTransactionAPI = new PaystackTransaction(secretKey);
+            var response = await paystackTransactionAPI.InitializeTransaction(model.Email, model.Amount, model.FirstName, model.LastName, "http://localhost:44329/order/callback");
+            
+                        
+            //Note that callback url is optional
+            if (response.status == true)
+            {
+                return Json(new { error = false, result = response }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { error = true, result = response }, JsonRequestBehavior.AllowGet);
+
+        }
+
+    }
+}
---
+++ Models/ViewModels/PaystackCustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PaystackPayment.Models.ViewModels
+{
+    public class PaystackCustomerModel
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Amount { get; set; }
+        public string Reference { get; set; }
+    }
+
+}
